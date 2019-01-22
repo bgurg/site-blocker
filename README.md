40d45565bd4d9fa41f14e713f983206575361888
bgurg <39531732+bgurg@users.noreply.github.com>
2019-01-22 13:34:19 -0800

    tweaked hostsfile

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # site-blocker
 - Idea of program is to run continually in the background and periodically wake up to check whether the current time is in working hours.  If so, add a list of distracting sites to the hosts file (if not already there) to be blocked.  Otherwise, remove these sites from the hosts file (if they are present) so that they become reachable again.  
  
-- To run against real hosts file, use approriate full path and run with administrator priviledges:
-    - Winows = C:\Windows\System32\drivers\etc\hosts
-    - Linux  = /etc/hosts
-
+- To run against real hosts file, use the approriate full path and run with administrator priviledges.
 - To run in background, change extension from .py to .pyw and run with pythonw
 
 - Written for Python Mega Course: Build 10 Real World Applications (Udemy)
--- main.py
+++ main.py
@@ -1,40 +1,27 @@
-# Idea of program is to run continually in the background and periodically 
-# wake up to check whether the current time is in working hours.  If so,
-# add a list of distracting sites to the hosts file (if not already there) to
-# be blocked.  Otherwise, remove these sites from the hosts file (if they are 
-# present) so that they become reachable again.  
-# 
-# The intention is to remove distractions by blocking the user from being able
-# to access a list of distracting sites during working hours.
-#
-# NOTE: To run this for real (i.e. instead of with a test hosts file),
-#       1) Backup the hosts file
-#       2) Rename script from .py to .pyw so it runs in background
-#       3) Make sure hostsfile includes full path where hosts file is located:
-#             - Winows = C:\Windows\System32\drivers\etc\hosts
-#             - Linux  = /etc/hosts
-#       4) Run .pyw file with administrator priviledges so that it is able
-#          to modify the hosts file
-
 import time # for sleep method
 from datetime import datetime as dt
 from datetime import date as date # for isoweekday method
 
 # hosts file information
-hostsfile = 'hosts_test_file'
+hostsfiles = {'test' : 'hosts_test_file',
+              'windows' : r'C:\Windows\System32\drivers\etc\hosts',
+              'linux' : '/etc/hosts'}
+hostsfile = hostsfiles['test']
 redirect = '127.0.0.1'
 
+# how long to sleep between checks
+sleep_duration_in_seconds = 1
+
 # list of sites to block during work hours
-sitelist = ['www.facebook.com','facebook.com','www.yahoo.com','yahoo.com']
+sitelist = ['www.facebook.com','facebook.com',
+            'finance.yahoo.com','www.yahoo.com', 'yahoo.com',
+            'news.google.com', 'finance.google.com']
 
 # work hours (e.g. 8:30 to 16:30)
 start_hr = 8
 start_min = 30
 end_hr = 16
 end_min = 30
-
-# how long to sleep between checks
-sleep_duration_in_seconds = 1
 
 def in_working_hours(now_dt):
     y, m, d = now_dt.year, now_dt.month, now_dt.day
@@ -52,14 +39,14 @@ if __name__ == "__main__":
         with open(hostsfile, 'r+') as f:
             if in_working_hours(dt.now()):
                 # add sites in sitelist to end of hostfile if not already there
-                print(f'.../{dt.now()}: In working hours.  Distracting sites blocked.')
+                print(f'.../{dt.now()}: In working hours - distracting sites blocked in "{hostsfile}"')
                 contents = f.read()
                 for site in sitelist:
                     if site not in contents:
                         f.write(f'{redirect}   {site}\n')                    
             else: 
                 # remove lines containing sites in sitelist from hostfile
-                print(f'.../{dt.now()}: Off work.  Surf all you want.')
+                print(f'.../{dt.now()}: Off work - surf all you want')
                 contents = f.readlines()
                 f.seek(0)
                 for line in contents:
